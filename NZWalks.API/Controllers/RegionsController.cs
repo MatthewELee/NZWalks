e7943667f700f600cfcdf76cdcd7780e1e2cb5c2
Matthew Lee <M.Lee@itpinternational.com>
2025-02-24 11:10:19 -0400

    DTO

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
 using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -20,34 +21,49 @@ namespace NZWalks.API.Controllers
          [HttpGet]
          public IActionResult GetAll()
          {
-            var regions = dbContext.Regions.ToList();
-            /*var regions = new List<Region>
+         /*var regions = new List<Region>
+            {
+               // GET ALL REGIONS
+               // GET: https: //localhost/portnumber/api/regions
+               new Region
                {
-                  // GET ALL REGIONS
-                  // GET: https: //localhost/portnumber/api/regions
-                  new Region
-                  {
-                     Id = Guid.NewGuid(),
-                     Name = "Auckland Region",
-                     Code = "AKL",
-                     RegionImageUrl = "https://www.pexels.com/photo/new-zealand-dockyard-18915023/"
-                  },
-                   new Region
-                  {
-                     Id = Guid.NewGuid(),
-                     Name = "Wellington Region",
-                     Code = "WLG",
-                     RegionImageUrl = "https://www.pexels.com/photo/red-rocks-new-zealand-17831396/"
-                  }
-               };
-         
-          
-     INSERT INTO  Regions
-     ([Id], [Code], [Name], [RegionImageUrl])
-     values ('d164ca1d-deb7-4a77-86c5-fb9a7148c835', 'AKL', 'Auckland', 'image.jpg')
-             */
+                  Id = Guid.NewGuid(),
+                  Name = "Auckland Region",
+                  Code = "AKL",
+                  RegionImageUrl = "https://www.pexels.com/photo/new-zealand-dockyard-18915023/"
+               },
+                new Region
+               {
+                  Id = Guid.NewGuid(),
+                  Name = "Wellington Region",
+                  Code = "WLG",
+                  RegionImageUrl = "https://www.pexels.com/photo/red-rocks-new-zealand-17831396/"
+               }
+            };
 
-            return Ok(regions);
+
+         //INSERT INTO  Regions
+         //([Id], [Code], [Name], [RegionImageUrl])
+         //values ('d164ca1d-deb7-4a77-86c5-fb9a7148c835', 'AKL', 'Auckland', 'image.jpg')
+          */
+
+         // Get Data From Database - Domain models
+         var regionsDomain = dbContext.Regions.ToList();
+
+         // Map Domain Models to DTOs
+         var regionDto = new List<RegionDto>();
+         foreach (var regionDomain in regionsDomain)
+         {
+            regionDto.Add(new RegionDto
+            {
+               Id = regionDomain.Id,
+               Code = regionDomain.Code,
+               Name = regionDomain.Name,
+               RegionImageUrl = regionDomain.RegionImageUrl
+            });
+         }
+         // Return DTO
+         return Ok(regionDto);
 
          }
          // GET SINGLE REGION (Get Region By ID)
@@ -57,12 +73,23 @@ namespace NZWalks.API.Controllers
          public IActionResult GetRegion(Guid id)
          {
          //var region = dbContext.Regions.Find(id);
-         var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
-         if (region == null)
+         // Get Region Domain Model From Database
+         var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+         // var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+         
+         if (regionDomain == null)
             {
                return NotFound();
             }
-            return Ok(region);
+         // Map/Conver Region Domain Model to Region DTO
+         var regionDto = new RegionDto
+         {
+            Id = regionDomain.Id,
+            Code = regionDomain.Code,
+            Name = regionDomain.Name,
+            RegionImageUrl = regionDomain.RegionImageUrl
+         };
+         return Ok(regionDto);
          }
    }
 }
