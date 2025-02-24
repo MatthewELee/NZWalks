ccf245447e1646c11671bdce2cca7098a6a10bcf
Matthew Lee <M.Lee@itpinternational.com>
2025-02-24 11:23:30 -0400

    POST RegionDTO

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -69,8 +69,8 @@ namespace NZWalks.API.Controllers
          // GET SINGLE REGION (Get Region By ID)
          // GET: https: //localhost/portnumber/api/regions/{id}
          [HttpGet]
-         [Route("{id}")]
-         public IActionResult GetRegion(Guid id)
+         [Route("{id:Guid}")]
+         public IActionResult GetRegion([FromRoute] Guid id)
          {
          //var region = dbContext.Regions.Find(id);
          // Get Region Domain Model From Database
@@ -91,5 +91,33 @@ namespace NZWalks.API.Controllers
          };
          return Ok(regionDto);
          }
+
+      // POST To Create New Region
+      // POST: https://localhost:portnumber/api/regions
+      [HttpPost]
+      public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+      {
+         // Map or convert DTO to Domain Model
+         var regionDomainModel = new Region
+         {
+            Code = addRegionRequestDto.Code,
+            Name = addRegionRequestDto.Name,
+            RegionImageUrl = addRegionRequestDto.RegionImageUrl
+         };
+         // Use Domain Model to create Region
+         dbContext.Regions.Add(regionDomainModel);
+         dbContext.SaveChanges();
+
+         //Map Domain Model back to DTO
+         var regionDto = new RegionDto
+         {
+            Id = regionDomainModel.Id,
+            Code = regionDomainModel.Code,
+            Name = regionDomainModel.Name,
+            RegionImageUrl = regionDomainModel.RegionImageUrl
+         };
+
+         return CreatedAtAction(nameof(GetRegion), new { id = regionDto.Id }, regionDto);
+      }
    }
 }
